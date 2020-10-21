fcb2e6cc0450a1070be9f81791949831ff3e39b3
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-21 13:46:53 +0530

    [Umesh]Added: catching exception for incorrect header

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -10,11 +10,12 @@ public class CensusAnalyserTest {
 	private static final String WRONG_CSV_FILE_PATH = "./src/main/java/resources/IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_TYPE = "./src/test/java/resources/IndiaStateCensusData.txt";
 	private static final String INCORRECT_DELIMETER_CSV_FILE = "./src/test/java/resources/IndiaStateCensusWrongData.csv";
-	private static final String INCORRECT_HEADER_INDIA_STATE_CSV_FILE = "./src/test/java/resources/IndiaStateCensusIncorrectHeaderData.csv";
+	private static final String INCORRECT_HEADER_CSV_FILE = "./src/test/java/resources/IndiaStateCensusIncorrectHeaderData.csv";
 	private static final String INDIA_STATE_CSV_FILE = "./src/test/java/resources/IndiaStateCode.csv";
 	private static final String WRONG_STATE_CODE_CSV_FILE_PATH = "./src/main/java/resources/IndiaStateCode.csv";
 	private static final String WRONG_INDIA_STATE_CSV_FILE_TYPE = "./src/test/java/resources/IndiaStateCode.txt";
 	private static final String INCORRECT_DELIMETER_INDIA_STATE_CSV_FILE = "./src/test/java/resources/IndiaStateCodeIncorrectDelimeter.csv";
+	private static final String INCORRECT_HEADER_INDIA_STATE_CSV_FILE = "./src/test/java/resources/IndiaStateCodeIncorrectHeader.csv";
 	
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -68,7 +69,7 @@ public class CensusAnalyserTest {
 			CensusAnalyser censusAnalyser = new CensusAnalyser();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyserException.class);
-			censusAnalyser.loadIndiaCensusData(INCORRECT_HEADER_INDIA_STATE_CSV_FILE);
+			censusAnalyser.loadIndiaCensusData(INCORRECT_HEADER_CSV_FILE);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.NOT_PROPER_CSV, e.type);
 		}
@@ -115,6 +116,18 @@ public class CensusAnalyserTest {
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyserException.class);
 			censusAnalyser.loadIndiaCensusData(INCORRECT_DELIMETER_INDIA_STATE_CSV_FILE);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.NOT_PROPER_CSV, e.type);
+		}
+	}
+	
+	@Test
+	public void givenIndiaStateCSV_WithWrongHeader_ShouldThrowException() {
+		try {
+			CensusAnalyser censusAnalyser = new CensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndiaCensusData(INCORRECT_HEADER_INDIA_STATE_CSV_FILE);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.NOT_PROPER_CSV, e.type);
 		}
